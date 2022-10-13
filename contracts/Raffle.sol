b2ea114d153520ee402c26c8a59db9ba069d07b7
Eugenio Pacelli Flores <flores.eugenio03@gmail.com>
2022-10-13 15:20:35 -0500

    getInterval added

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -212,4 +212,8 @@ abstract contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
     function getRequestConfirmations() public pure returns (uint256) {
         return REQUEST_CONFIRMATIONS;
     }
+
+    function getInterval() public view returns (uint256) {
+        return i_interval;
+    }
 }
